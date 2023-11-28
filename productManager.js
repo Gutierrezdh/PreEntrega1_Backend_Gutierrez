9489d3a60deaa1156315d3fc32ecc9ddf828ecf2
Gutierrezdh <Gutierrezdh@gmail.com>
2023-11-28 20:15:43 -0300

    Subo cambios de pre-entrega 2

--- productManager.js
+++ productManager.js
@@ -1,23 +1,50 @@
+const fs = require('fs');
+const FILE_NAME = "./products.json";
+
 class ProductManager {
-    constructor() {
-    this.products = [];
-    this.productIdCounter = 1;
+    constructor(filepath) {
+        this.path = FILE_NAME;
+        this.products = [];
+        this.productIdCounter = 1;
+        this.initializeFile();
+    }
+
+    initializeFile() {
+        try {
+            const data = fs.readFileSync(this.path, 'utf8');
+            this.products = JSON.parse(data);
+            if (this.products.length > 0) {
+                const lastProduct = this.products[this.products.length - 1];
+                this.productIdCounter = lastProduct.id + 1;
+            }
+        } catch (error) {
+            // Si el archivo no existe o esta vacío continúo con un array vacío
+            this.products = [];
+        }
+    }
+
+    saveToFile() {
+        try {
+            fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2));
+        } catch (error) {
+            console.error('Error al guardar el archivo:', error);
+        }
     }
 
     addProduct(product) {
         const { title, description, price, thumbnail, code, stock } = product;
-        // Valido los campos obligatorios que debe tener cada producto
+
         if (!title || !description || !price || !thumbnail || !code || !stock) {
             console.error('Todos los campos son obligatorios.');
             return;
         }
-        // Valido que no se repita el campo Code
+
         const codeExists = this.products.some((prod) => prod.code === code);
         if (codeExists) {
             console.error('Ya existe un producto con este código.');
             return;
         }
-        // Agrego producto e incremento el id
+
         const newProduct = {
             id: this.productIdCounter,
             title,
@@ -29,26 +56,47 @@ class ProductManager {
         };
         this.products.push(newProduct);
         this.productIdCounter++;
+        this.saveToFile();
         console.log('Producto agregado:', newProduct);
     }
 
     getProducts() {
-    return this.products;
+        return this.products;
     }
 
     getProductById(id) {
-    const product = this.products.find((prod) => prod.id === id);
-    if (product) {
-        return product;
-    } else {
-        console.error('Not found');
+        const product = this.products.find((prod) => prod.id === id);
+        if (product) {
+            return product;
+        } else {
+            console.error('Producto no encontrado');
+        }
     }
+
+    updateProduct(id, updatedFields) {
+        const index = this.products.findIndex((product) => product.id === id);
+        if (index !== -1) {
+            this.products[index] = { ...this.products[index], ...updatedFields };
+            this.saveToFile();
+            return this.products[index];
+        } else {
+            console.error('Producto no encontrado');
+        }
+    }
+
+    deleteProduct(id) {
+        this.products = this.products.filter((product) => product.id !== id);
+        this.saveToFile(); // No esperará a que se complete la operación de guardado
+        console.log('Producto eliminado');
     }
 }
+
+module.exports = ProductManager;
 
   // Codigo para probar metodos
 
 /*
+
 const manager = new ProductManager();
 
 manager.addProduct({
@@ -75,4 +123,17 @@ console.log('getProducts:', allProducts);
 const productById = manager.getProductById(2);
 console.log('getProductById:', productById);
 
+// Eliminación 
+const productosAntes = manager.getProducts();
+console.log('Productos antes de la eliminación:', productosAntes);
+
+// Eliminar un producto por su ID (por ejemplo, ID 1)
+manager.deleteProduct(2);
+
+// Obtener productos después de la eliminación
+const productosDespues = manager.getProducts();
+console.log('Productos despues de la eliminación:', productosDespues);
+
 */
+
+
